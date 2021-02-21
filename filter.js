22b1ec5c6e4e00c43b81b2af0d9c417f81f31b50
ThomLam <tlambert@kemptechnologies.com>
2021-02-21 02:05:54 +0000

    Rewrite filter to use MutationObeserver and ES6 syntax.
    Adds mutationObserver to monitor results DOM instead of listener events.
    Short-circuit loops where possible to reduce processing time.
    Clean up debug comments and statements.

--- filter.js
+++ filter.js
@@ -1,33 +1,53 @@
-console.log("myextension loaded");
+console.log("filter extension loaded");
+
 // dump some defaults into browser storage
 browser.storage.local.set(
-    { "filteredDomains": ["javascriptinfo.com"] }
-).then(() => {
-    console.log("populated test defaults into storage")
-}
+    { "filteredDomains": ["stackoverflow.com"] }
 );
 
-
-let storage = browser.storage.local.get("filteredDomains");
-//promise returned
-storage.then((results) => {
-    console.log("results");
-    console.log(results);
-    if (!results.filteredDomains) {
-        results.filteredDomains = [];
-    }
-    filteredDomains = results.filteredDomains;
-    console.log("Filtered domains is:");
-    console.log(filteredDomains);
-    //listen for updates on the search result page
-    document.querySelector("#links").addEventListener("DOMSubtreeModified", () => {
-        filteredDomains.forEach((domain) => {
-            //match all elements whose data-domain attribute end with our target
-            document.querySelectorAll(
-                "div[data-domain$='" + domain + "']"
-            ).forEach((el) => {
-                el.style.backgroundColor = "red";
+function filterResults(mutationsList) {
+    browser.storage.local.get("filteredDomains").then(
+        function (storedData) {
+            if (!storedData.filteredDomains) {
+                results.filteredDomains = [];
+            }
+            filteredDomains = storedData.filteredDomains;
+            mutationsList.forEach((mutation) => {
+                if (mutation.type === 'childList') { //a child node was added
+                    mutation.addedNodes.forEach((node) => {
+                        if (node.classList.contains('result')) {
+                            let liveNode = document.querySelector("#" + node.id);
+                            for (let blacklistDomain of filteredDomains) {
+                                let liveNode = document.querySelector("#" + node.id);
+                                matchedNode = liveNode.dataset.domain;
+                                if (matchedNode && matchedNode.includes(blacklistDomain)) {
+                                    wrapAndCollapseResult(liveNode);
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    );
+                }
             });
         });
-    });
-});
+}
+
+function wrapAndCollapseResult(resultNode) {
+    /* Wrap the blacklisted result with a <details> element so we can autocollapse it */
+    let wrapper = document.createElement('details');
+    let filterSummary = document.createElement('summary');
+    filterSummary.textContent = "Filtered result";
+    wrapper.appendChild(filterSummary);
+    resultNode.parentNode.insertBefore(wrapper, resultNode);
+    wrapper.appendChild(resultNode);
+}
+
+
+let resultsObserver = new MutationObserver(filterResults);
+let resultDiv = document.querySelector('#links');
+/*
+register the observer to watch for child node changes on the results div 
+that are nodes with a data-domain element.
+*/
+resultsObserver.observe(resultDiv, { childList: true, attributeFilter: ['result', 'data-domain'] });
